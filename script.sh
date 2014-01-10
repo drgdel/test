16d74b91c6f25c3120793d525df54e2d70f864ef
drgdel <drgdel@yopmail.com>
2014-01-10 13:15:03 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 
 echo "deb http://ftp.us.debian.org/debian/ jessie main contrib" >> /etc/apt/sources.list &&
 apt-get update &&
-apt-get dist-upgrade &&
+apt-get dist-upgrade -y  &&
 apt-get install sudo git &&
 addgroup user sudo  &&
 git clone https://github.com/adrelanos/Whonix &&
